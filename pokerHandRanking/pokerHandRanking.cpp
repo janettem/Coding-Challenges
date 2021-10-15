18945a0735ee8725c26b0576d88bcbfce7133b1e
janettem <janette.metelinen@gmail.com>
2021-10-15 13:50:09 +0000

    Add recognition for royal flush

--- pokerHandRanking/pokerHandRanking.cpp
+++ pokerHandRanking/pokerHandRanking.cpp
@@ -1,24 +1,63 @@
+// https://edabit.com/challenge/EZKbj4mM3xEfDpb5c
+
 #include <string>
 #include <iostream>
 
-using namespace std;
-
-string getPokerHandRanking(string hand[5]);
+std::string getPokerHandRanking(std::string hand[5]);
+bool isRoyalFlush(std::string hand[5]);
+bool isSameSuit(std::string hand[5]);
+char getSuit(std::string card);
+std::string getRank(std::string card);
 
 int main()
 {
-    string royalFlush[5] = {"10h", "Jh", "Qh", "Ah", "Kh"};
-    string highCard[5] = {"3h", "5h", "Qs", "9h", "Ad"};
+    std::string royalFlush[5] = {"10h", "Jh", "Qh", "Ah", "Kh"};
+    std::string highCard[5] = {"3h", "5h", "Qs", "9h", "Ad"};
 
-    cout << (getPokerHandRanking(royalFlush) == "Royal Flush");
-    cout << (getPokerHandRanking(highCard) == "High Card");
+    std::cout << (getPokerHandRanking(royalFlush) == "Royal Flush");
+    std::cout << (getPokerHandRanking(highCard) == "High Card");
 
-    cout << endl;
+    std::cout << std::endl;
 
     return 0;
 }
 
-string getPokerHandRanking(string hand[5])
+std::string getPokerHandRanking(std::string hand[5])
 {
-    return "Royal Flush";
+    if (isRoyalFlush(hand))
+        return "Royal Flush";
+    return "High Card";
+}
+
+bool isRoyalFlush(std::string hand[5])
+{
+    if (isSameSuit(hand) &&
+        getRank(hand[0]) == "A" &&
+        getRank(hand[1]) == "K" &&
+        getRank(hand[2]) == "Q" &&
+        getRank(hand[3]) == "J" &&
+        getRank(hand[4]) == "10")
+        return true;
+    return false;
+}
+
+bool isSameSuit(std::string hand[5])
+{
+    char suit = getSuit(hand[0]);
+    if (getSuit(hand[1]) == suit &&
+        getSuit(hand[2]) == suit &&
+        getSuit(hand[3]) == suit &&
+        getSuit(hand[4]) == suit)
+        return true;
+    return false;
+}
+
+char getSuit(std::string card)
+{
+    return card[card.length() - 1];
+}
+
+std::string getRank(std::string card)
+{
+    return card.substr(0, card.length() - 1);
 }
