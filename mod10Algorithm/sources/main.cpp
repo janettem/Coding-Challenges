a97ebc35a829c8e84d0ba9b0f55d862f220670cb
janettem <janette.metelinen@gmail.com>
2021-10-25 07:56:38 +0000

    Reorder functions

--- mod10Algorithm/sources/main.cpp
+++ mod10Algorithm/sources/main.cpp
@@ -1,19 +1,6 @@
 #include "mod10Algorithm.h"
 #include <iostream>
 
-/* https://en.wikipedia.org/wiki/Luhn_algorithm
-
-0 ->  0 -> 0
-1 ->  2 -> 2
-2 ->  4 -> 4
-3 ->  6 -> 6
-4 ->  8 -> 8
-5 -> 10 -> 1
-6 -> 12 -> 3
-7 -> 14 -> 5
-8 -> 16 -> 7
-9 -> 18 -> 9 */
-
 int main()
 {
     std::cout << (isValidCreditCard("2111111111121111") == false);
--- mod10Algorithm/sources/mod10Algorithm.cpp
+++ mod10Algorithm/sources/mod10Algorithm.cpp
@@ -1,21 +1,33 @@
 #include "mod10Algorithm.h"
 
-std::string getOriginalNumber(std::string number)
+static std::string getOriginalNumber(std::string number);
+static std::string *doubleEveryOtherDigit(std::string number);
+static std::string doubleDigit(char digit);
+static std::string sumDigits(std::string *number, int size);
+static char sumDigits2(std::string number);
+static int sumDigits3(std::string number);
+static int charToInt(char c);
+static char calculateCheckDigit(int sum);
+
+bool isValidCreditCard(std::string number)
+{
+    std::string originalNumber, *everyOtherDigitDoubled, digitsSummed;
+    int sum, calculatedCheckDigit;
+    originalNumber = getOriginalNumber(number);
+    everyOtherDigitDoubled = doubleEveryOtherDigit(originalNumber);
+    digitsSummed = sumDigits(everyOtherDigitDoubled, originalNumber.length());
+    sum = sumDigits3(digitsSummed);
+    calculatedCheckDigit = calculateCheckDigit(sum);
+    delete[] everyOtherDigitDoubled;
+    return calculatedCheckDigit == number[number.length() - 1];
+}
+
+static std::string getOriginalNumber(std::string number)
 {
     return number.substr(0, number.length() - 1);
 }
 
-int charToInt(char c)
-{
-    return c - '0';
-}
-
-std::string doubleDigit(char digit)
-{
-    return std::to_string(charToInt(digit) * 2);
-}
-
-std::string *doubleEveryOtherDigit(std::string number)
+static std::string *doubleEveryOtherDigit(std::string number)
 {
     std::string *everyOtherDigitDoubled = new std::string[number.length()];
     bool everyOtherDigit = true;
@@ -30,14 +42,12 @@ std::string *doubleEveryOtherDigit(std::string number)
     return everyOtherDigitDoubled;
 }
 
-char sumDigits2(std::string number)
+static std::string doubleDigit(char digit)
 {
-    if (number.length() == 2)
-        return std::to_string(charToInt(number[0]) + charToInt(number[1]))[0];
-    return number[0];
+    return std::to_string(charToInt(digit) * 2);
 }
 
-std::string sumDigits(std::string *number, int size)
+static std::string sumDigits(std::string *number, int size)
 {
     std::string digitsSummed;
     for (int i = 0; i < size; i++)
@@ -45,7 +55,14 @@ std::string sumDigits(std::string *number, int size)
     return digitsSummed;
 }
 
-int sumDigits3(std::string number)
+static char sumDigits2(std::string number)
+{
+    if (number.length() == 2)
+        return std::to_string(charToInt(number[0]) + charToInt(number[1]))[0];
+    return number[0];
+}
+
+static int sumDigits3(std::string number)
 {
     int sum = 0;
     for (int i = 0; i < number.length(); i++)
@@ -53,20 +70,12 @@ int sumDigits3(std::string number)
     return sum;
 }
 
-char calculateCheckDigit(int sum)
+static int charToInt(char c)
 {
-    return std::to_string(10 - sum % 10)[0];
+    return c - '0';
 }
 
-bool isValidCreditCard(std::string number)
+static char calculateCheckDigit(int sum)
 {
-    std::string originalNumber, *everyOtherDigitDoubled, digitsSummed;
-    int sum, calculatedCheckDigit;
-    originalNumber = getOriginalNumber(number);
-    everyOtherDigitDoubled = doubleEveryOtherDigit(originalNumber);
-    digitsSummed = sumDigits(everyOtherDigitDoubled, originalNumber.length());
-    sum = sumDigits3(digitsSummed);
-    calculatedCheckDigit = calculateCheckDigit(sum);
-    delete[] everyOtherDigitDoubled;
-    return calculatedCheckDigit == number[number.length() - 1];
+    return std::to_string(10 - sum % 10)[0];
 }
