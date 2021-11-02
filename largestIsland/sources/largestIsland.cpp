3b1838c22208db029b1fc42d17116a3393867bc6
janettem <janette.metelinen@gmail.com>
2021-11-02 08:50:27 +0000

    Rename functions and variables in largestIsland

--- largestIsland/sources/largestIsland.cpp
+++ largestIsland/sources/largestIsland.cpp
@@ -8,13 +8,13 @@ enum Map
 
 static int **copyMatrix(std::vector<std::vector<int>> matrix);
 static int *copyRow(std::vector<int> row);
-static int countIslandSizeAndErase(int **map, int rows, int columns, int y, int x);
+static int getIslandAreaAndErase(int **map, int rows, int columns, int y, int x);
 static void deleteMatrix(int **matrix, int size);
 
 int getLargestIslandArea(std::vector<std::vector<int>> map)
 {
-    int largestSize = 0;
-    int size;
+    int largestArea = 0;
+    int area;
     int **copy = copyMatrix(map);
     int rows = map.size();
     int columns = map[0].size();
@@ -24,14 +24,14 @@ int getLargestIslandArea(std::vector<std::vector<int>> map)
         {
             if (copy[y][x] == Land)
             {
-                size = countIslandSizeAndErase(copy, rows, columns, y, x);
-                if (size > largestSize)
-                    largestSize = size;
+                area = getIslandAreaAndErase(copy, rows, columns, y, x);
+                if (area > largestArea)
+                    largestArea = area;
             }
         }
     }
     deleteMatrix(copy, rows);
-    return largestSize;
+    return largestArea;
 }
 
 static int **copyMatrix(std::vector<std::vector<int>> matrix)
@@ -52,19 +52,19 @@ static int *copyRow(std::vector<int> row)
     return copy;
 }
 
-static int countIslandSizeAndErase(int **map, int rows, int columns, int y1, int x1)
+static int getIslandAreaAndErase(int **map, int rows, int columns, int y1, int x1)
 {
-    int islandSize = 1;
+    int area = 1;
     map[y1][x1] = Water;
     for (int y2 = (y1 - 1 < 0 ? 0 : y1 - 1); y2 < (y1 + 2 > rows ? rows : y1 + 2); y2++)
     {
         for (int x2 = (x1 - 1 < 0 ? 0 : x1 - 1); x2 < (x1 + 2 > columns ? columns : x1 + 2); x2++)
         {
             if (map[y2][x2] == Land)
-                islandSize += countIslandSizeAndErase(map, rows, columns, y2, x2);
+                area += getIslandAreaAndErase(map, rows, columns, y2, x2);
         }
     }
-    return islandSize;
+    return area;
 }
 
 static void deleteMatrix(int **matrix, int size)
